e788a14ebb6b46513ade9b7fd16c20ef0758fdd8
Adria331 <adria331133@gmail.com>
2017-03-02 17:24:18 +0100

    Add files via upload

--- FreeBookSearch.py
+++ FreeBookSearch.py
@@ -8,7 +8,14 @@ Free book name searcher
 
 import urllib2
 from bs4 import BeautifulSoup
-  
+import subprocess
+def start(bot, update):
+    update.message.reply_text('Hello World!')
+
+def hello(bot, update):
+    update.message.reply_text(
+        'Hello {}'.format(update.message.from_user.first_name))
+
 class Client(object):
 
     def get_web(self, link):
@@ -17,23 +24,25 @@ class Client(object):
         file.close()
         return html
 
-    # Search the daily free book name
+    # Search the daily free book name of Packt website
     def search_book_name(self, html):
         soup = BeautifulSoup(html, 'html.parser')
         elements = soup.find_all("div", "dotd-title")
         bookName = elements[0].find_all("h2")[0].text.strip()
         return bookName
 
+    # Shows a system message with the name of the book 
+    #(You can use cron to show it always)
+    def sendMessage(self, book):
+    	subprocess.Popen(['notify-send', "The free book of the day is: " + book])
+
     def main(self):
         htmlWeb = self.get_web(
           'https://www.packtpub.com/packt/offers/free-learning/')
         freeBook = self.search_book_name(htmlWeb)
         print freeBook
+        self.sendMessage(str(freeBook))
 
-    # Look if element is a tag
-    def identifyElement(self, element):
-        return '[' not in element and '<' not in element \
-                and '>' not in element and ']' not in element
 
 if __name__ == "__main__":
     clientWeb = Client()
